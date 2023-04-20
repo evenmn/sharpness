dc76276f3961536343f166a9bb909c7e86d8e8ce
John Schreck <jsschreck@gmail.com>
2023-04-20 11:42:21 -0600

    Linting, added comments

--- src/transforms.py
+++ src/transforms.py
@@ -34,7 +34,6 @@ def LoadTransformations(transform_config: str):
     if "GaussianBlur" in transform_config:
         rate = transform_config["GaussianBlur"]["rate"]
         k_sz = transform_config["GaussianBlur"]["kernel_size"]
-        sigma = transform_config["GaussianBlur"]["sigma"]
         if rate > 0.0:
             tforms.append(GaussianBlur(rate, k_sz, brightness))
     if "RandomCrop" in transform_config:
@@ -43,10 +42,10 @@ def LoadTransformations(transform_config: str):
 
 
 class RandVerticalFlip(object):
-
     def __init__(self, rate):
         logging.info(
-            f"Loaded RandomVerticalFlip transformation with probability {rate}")
+            f"Loaded RandomVerticalFlip transformation with probability {rate}"
+        )
         self.rate = rate
 
     def __call__(self, image):
@@ -56,10 +55,10 @@ class RandVerticalFlip(object):
 
 
 class RandHorizontalFlip(object):
-
     def __init__(self, rate):
         logging.info(
-            f"Loaded RandomHorizontalFlip transformation with probability {rate}")
+            f"Loaded RandomHorizontalFlip transformation with probability {rate}"
+        )
         self.rate = rate
 
     def __call__(self, image):
@@ -80,8 +79,7 @@ class Rescale(object):
     def __init__(self, output_size):
         assert isinstance(output_size, (int, tuple))
         self.output_size = output_size
-        logger.info(
-            f"Loaded Rescale transformation with output size {output_size}")
+        logger.info(f"Loaded Rescale transformation with output size {output_size}")
 
     def __call__(self, image):
         image_dim = image.shape
@@ -95,7 +93,7 @@ class Rescale(object):
         image = rescale(image, frac, anti_aliasing=False)
         image = image.reshape(1, image.shape[-2], image.shape[-1])
 
-        return sample
+        return image
 
 
 class RandomCrop(object):
@@ -113,26 +111,20 @@ class RandomCrop(object):
         else:
             assert len(output_size) == 2
             self.output_size = output_size
-        logger.info(f"Loaded RandomCrop transformation")
+        logger.info("Loaded RandomCrop transformation")
 
-    def __call__(self, sample):
-        image = sample['image']
-
+    def __call__(self, image):
         h, w = image.shape[:2]
         new_h, new_w = self.output_size
 
         top = np.random.randint(0, h - new_h)
         left = np.random.randint(0, w - new_w)
 
-        image = image[top: top + new_h,
-                      left: left + new_w]
-
-        sample["image"] = image
-        return sample
+        image = image[top : top + new_h, left : left + new_w]
+        return image
 
 
 class AdjustBrightness(object):
-
     def __init__(self, rate, brightness):
         self.rate = rate
         self.brightness = brightness
@@ -159,19 +151,18 @@ class AdjustBrightness(object):
 
 
 class GaussianBlur(object):
-
     def __init__(self, rate, sigma):
         self.rate = rate
         self.sigma = sigma
 
     def __call__(self, image):
-        
+
         if random.random() < self.rate:
             image = self.gaussian_blur(image, self.sigma)
-            
+
         # Clip the values to the valid range of [0, 255]
         image = np.clip(image, 0, 255)
-            
+
         return image
 
     def gaussian_blur(self, image, sigma):
@@ -187,94 +178,94 @@ class GaussianBlur(object):
         return image
 
 
-
 class GaussianNoise(object):
-
     def __init__(self, rate, noise):
-        logging.info(
-            f"Loaded GaussianNoise transformation")
+        logging.info("Loaded GaussianNoise transformation")
         self.rate = rate
         self.noise = noise
 
     def __call__(self, image):
-        
+
         # Convert the image to a numpy array
-        image = np.array(image, dtype = float)
-        
+        image = np.array(image, dtype=float)
+
         if random.random() < self.rate:
             noise = np.random.normal(0, self.noise, image.shape)
             image += noise
-            
+
         # Clip the values to the valid range of [0, 255]
         image = np.clip(image, 0, 255)
-            
+
         return image
-    
-    
-    
+
+
 if __name__ == "__main__":
     import requests
     from io import BytesIO
     from PIL import Image
     import matplotlib.pyplot as plt
-    
+
     # Download the image
-    response = requests.get("https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/1280px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg")
+    response = requests.get(
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/1280px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg"
+    )
     img = Image.open(BytesIO(response.content))
 
     # Apply the GaussianBlur transformation to the image
     blurred_img = GaussianBlur(rate=1, sigma=5)(img)
 
     # Apply the GaussianNoise transformation to the image
-    noise_img = GaussianNoise(rate = 1, noise = 150)(img)
+    noise_img = GaussianNoise(rate=1, noise=150)(img)
 
     # Apply the Brightness adjustment transformation to the image
-    bright_img = AdjustBrightness(rate = 1, brightness = 2.6)(img)
+    bright_img = AdjustBrightness(rate=1, brightness=2.6)(img)
 
     # Apply the H-flip transformation to the image
-    hflip_img = RandHorizontalFlip(rate = 1)(img)
+    hflip_img = RandHorizontalFlip(rate=1)(img)
 
     # Apply the V-flip transformation to the image
-    vflip_img = RandVerticalFlip(rate = 1)(img)
+    vflip_img = RandVerticalFlip(rate=1)(img)
 
     # Create a figure with two subplots
-    fig, axs = plt.subplots(nrows=5, ncols=2, figsize=(5, 10), sharex = "col", sharey = "row")
+    fig, axs = plt.subplots(
+        nrows=5, ncols=2, figsize=(5, 10), sharex="col", sharey="row"
+    )
 
     # Plot the original image on the first subplot
     axs[0][0].imshow(img)
-    axs[0][0].set_title('Original')
+    axs[0][0].set_title("Original")
 
     # Plot the blurred image on the second subplot
     axs[0][1].imshow(blurred_img)
-    axs[0][1].set_title('Blurred')
+    axs[0][1].set_title("Blurred")
 
     # Gaussian Noise
     axs[1][0].imshow(img)
-    axs[1][0].set_title('Original')
+    axs[1][0].set_title("Original")
 
-    axs[1][1].imshow(noise_img.astype('uint8'))
-    axs[1][1].set_title('Background noise')
+    axs[1][1].imshow(noise_img.astype("uint8"))
+    axs[1][1].set_title("Background noise")
 
     # Brightness
     axs[2][0].imshow(img)
-    axs[2][0].set_title('Original')
+    axs[2][0].set_title("Original")
 
-    axs[2][1].imshow(bright_img.astype('uint8'))
-    axs[2][1].set_title('Brightness adjusted')
+    axs[2][1].imshow(bright_img.astype("uint8"))
+    axs[2][1].set_title("Brightness adjusted")
 
     # H-flip
     axs[3][0].imshow(img)
-    axs[3][0].set_title('Original')
+    axs[3][0].set_title("Original")
 
-    axs[3][1].imshow(hflip_img.astype('uint8'))
-    axs[3][1].set_title('Horizontally flipped')
+    axs[3][1].imshow(hflip_img.astype("uint8"))
+    axs[3][1].set_title("Horizontally flipped")
 
     # V-flip
     axs[4][0].imshow(img)
-    axs[4][0].set_title('Original')
+    axs[4][0].set_title("Original")
 
-    axs[4][1].imshow(vflip_img.astype('uint8'))
-    axs[4][1].set_title('Vertically flipped')
+    axs[4][1].imshow(vflip_img.astype("uint8"))
+    axs[4][1].set_title("Vertically flipped")
 
     # Show the figure
     plt.tight_layout()
