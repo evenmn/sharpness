2042b50c3341c8cd5dd8c9e38b50b690bb2aa9f0
stockeh <jason12stock@gmail.com>
2023-04-20 05:44:30 -0600

    evaluated metrics added

--- src/benchmark.py
+++ src/benchmark.py
@@ -1,5 +1,7 @@
 import argparse
 
+from metrics import compute_metric, compute_all_metrics
+
 parser = argparse.ArgumentParser(description='Sharpness Benchmarks')
 
 parser.add_argument('--fakedata', action='store_true',
@@ -9,6 +11,9 @@ parser.add_argument('--data-filename', type=str, default='../data/something.npy'
                     help='name of file to load data from')
 parser.add_argument('--output-filename', type=str, default='../media/output.jpg',
                     help='name of output file visualization to save data')
+
+parser.add_argument('--metric', type=str, default='all',
+                    help='evaluation metric to compute')
 
 
 def visualize(data, output_filename=None):
@@ -25,27 +30,37 @@ def load_real_data():
 
 def load(args):
     if args.fakedata:
-        x = generate_synthetic_data()
+        X = generate_synthetic_data()
     else:
-        x = load_real_data(args.data_filename)
-    return x
+        X = load_real_data(args.data_filename)
+    return X
 
 
-def transform(x):
-    t = None
-    return t
+def transform(X):
+    T = None
+    return T
 
 
-def eval(x, t):
-    pass
+def evaluate(X, T, metric):
+    if metric == 'all':
+        results = compute_all_metrics(X, T)
+        for metric, result in results.items():
+            print(f'{metric}: {result}')
+    else:
+        metric_value = compute_metric(X, T, metric)
+        print(f'{metric}: {metric_value}')
 
 
 def main(args):
-    x = load(args)
-    t = transform(x)
-    metrics = eval(x, t)
+    X = load(args)
+    T = transform(X)
+    metrics = evaluate(X, T)
 
     data = dict()
+    data['X'] = X
+    data['T'] = T
+    data['metrics'] = metrics
+
     visualize(data, args.output_filename)
 
 
---
+++ src/metrics.py
@@ -0,0 +1,62 @@
+import numpy as np
+from skimage import filters
+
+
+def mse(X, T):
+    """Mean Squared Error"""
+    return np.mean((X - T) ** 2)
+
+
+def mae(X, T):
+    """Mean Absolute Error"""
+    return np.mean(np.abs((X - T)))
+
+
+def rmse(X, T):
+    """Root Mean Squared Error"""
+    return np.sqrt(mse(X, T))
+
+
+def grad(X, T):
+    """Average Magnitude of the Gradient
+
+    Edge magnitude is computed as:
+        sqrt(Gx^2 + Gy^2)
+    """
+    def _f(x): return np.mean(filters.sobel(x))
+    return (_f(X), _f(T))
+
+
+######### Evaluation Metrics #########
+metric_f = {
+    'mse': mse,
+    'mae': mae,
+    'rmse': rmse,
+    'grad': grad,
+}
+
+
+def compute_all_metrics(X, T) -> dict:
+    """Compute all evaluation metrics."""
+    results = dict()
+    for metric, f in metric_f.items():
+        results[metric] = f(X, T)
+    return results
+
+
+def compute_metric(X, T, metric: str):
+    """Compute specified evaluation metric"""
+    f = metric_f.get(metric)
+    if f is None:
+        raise ValueError(f'Unknown metric name: {metric}')
+    return f(X, T)
+
+
+if __name__ == '__main__':
+    from skimage.data import camera
+    X = camera()
+    T = np.fliplr(X)
+
+    results = compute_all_metrics(X, T)
+    for metric, result in results.items():
+        print(f'{metric}: {result}')
