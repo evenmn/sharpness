1ee67e648a2313e7e3713475d7529bd4d58ccd97
Lander Ver Hoef <lander.ver_hoef@colostate.edu>
2023-10-31 21:00:36 +0000

    Added heatmap functions; moved list of metrics to separate file

---
+++ src/metric_list.py
@@ -0,0 +1,50 @@
+from sharpness.metrics import (
+    mse,
+    mae,
+    rmse,
+    grad,
+    s1,
+    total_variation
+)
+from sharpness.gradient import (
+    psnr,
+    normalized_cross_correlation,
+    gradient_difference_similarity,
+    gradient_rmse,
+    laplacian_rmse,
+    histogram_intersection,
+    hog_pearson,
+    grad_total_variation
+)
+from sharpness.fourier import (
+    fourier_image_similarity,
+    fourier_total_variation
+)
+from sharpness.wavelet import (
+    wavelet_image_similarity,
+    wavelet_total_variation
+)
+
+metric_f = {
+    'mse': mse,
+    'mae': mae,
+    'rmse': rmse,
+    's1': s1,
+    "psnr": psnr,
+    "ncc": normalized_cross_correlation,
+    'grad': grad,
+    "grad-ds": gradient_difference_similarity,
+    "grad-rmse": gradient_rmse,
+    "laplace-rmse": laplacian_rmse,
+    "hist-int": histogram_intersection,
+    #"gpd": gradient_profile_difference,
+    "hog-pearson": hog_pearson,
+    "fourier-similarity": fourier_image_similarity,
+    "wavelet-similarity": wavelet_image_similarity,
+    "tv": total_variation,
+    "grad-tv": grad_total_variation,
+    "fourier-tv": fourier_total_variation,
+    "wavelet-tv": wavelet_total_variation
+}
+
+single_metrics = ["grad", "s1", "tv", "grad-tv", "fourier-tv", "wavelet-tv"]
