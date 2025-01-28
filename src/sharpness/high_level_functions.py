6408559bf3fc8759cd46aab828deb8e8fc67e43c
Lander Ver Hoef <lander.ver_hoef@colostate.edu>
2025-01-28 00:39:51 +0000

    Updated docstrings

--- src/sharpness/high_level_functions.py
+++ src/sharpness/high_level_functions.py
@@ -1,10 +1,15 @@
+"""Module containing high level routines"""
 import numpy as np
 from .metrics import metric_f, single_metrics
 from .heatmap import Heatmap, heatmap_list
 
 
-def compute_all_metrics_globally(X, T) -> dict:
-    """Check that X and T are 2-dim arrays"""
+def compute_all_metrics_globally(X: np.ndarray, T: np.ndarray) -> dict:
+    """
+    High-level function to compute all metrics between X and T
+
+    Note that X and T must be 2D arrays
+    """
     assert (
         np.ndim(X) == 2
     ), f"Input must be 2-dimensional; got {np.ndim(X)} dimensions for X"
@@ -13,7 +18,7 @@ def compute_all_metrics_globally(X, T) -> dict:
             np.ndim(T) == 2
         ), f"Input must be 2-dimensional; got {np.ndim(T)} dimensions for T"
 
-    """Compute all evaluation metrics."""
+    # Compute all evaluation metrics.
     results = dict()
     metrics_to_compute = single_metrics if T is None else metric_f.keys()
 
@@ -37,7 +42,11 @@ def compute_all_metrics_globally(X, T) -> dict:
 
 
 def compute_metric_globally(X, T, metric: str):
-    """Check that X and T are 2-dim arrays"""
+    """
+    High-level function to compute a single metric between X and T
+
+    Note that X and T must be 2D arrays
+    """
     assert (
         np.ndim(X) == 2
     ), f"Input must be 2-dimensional; got {np.ndim(X)} dimensions for X"
@@ -46,7 +55,7 @@ def compute_metric_globally(X, T, metric: str):
             np.ndim(T) == 2
         ), f"Input must be 2-dimensional; got {np.ndim(T)} dimensions for T"
 
-    """Compute specified evaluation metric"""
+    # Compute specified evaluation metric
     f = metric_f.get(metric)
     if f is None:
         raise ValueError(f"Unknown metric name: {metric}")
@@ -63,7 +72,11 @@ def compute_metric_globally(X, T, metric: str):
 def compute_all_metrics_locally(
     X, T, block_size=None, pad_len=None, verbose=True
 ) -> dict:
-    """Check that X and T are 2-dim arrays"""
+    """
+    High-level function to compute all metric heatmaps between X and T
+
+    Note that X and T must be 2D arrays
+    """
     assert (
         np.ndim(X) == 2
     ), f"Input must be 2-dimensional; got {np.ndim(X)} dimensions for X"
@@ -72,7 +85,7 @@ def compute_all_metrics_locally(
             np.ndim(T) == 2
         ), f"Input must be 2-dimensional; got {np.ndim(T)} dimensions for T"
 
-    """Compute all evaluation metrics."""
+    # Compute all evaluation metrics."""
     metrics_to_compute = single_metrics if T is None else metric_f.keys()
     if block_size is None:
         block_size = X.shape[0] // 8
@@ -98,7 +111,11 @@ def compute_all_metrics_locally(
 def compute_metric_locally(
     X, T, metric: str, block_size=None, pad_len=None, verbose=True
 ):
-    """Check that X and T are 2-dim arrays"""
+    """
+    High-level function to compute a single metric heatmap between X and T
+
+    Note that X and T must be 2D arrays
+    """
     assert (
         np.ndim(X) == 2
     ), f"Input must be 2-dimensional; got {np.ndim(X)} dimensions for X"
@@ -107,7 +124,7 @@ def compute_metric_locally(
             np.ndim(T) == 2
         ), f"Input must be 2-dimensional; got {np.ndim(T)} dimensions for T"
 
-    """Compute specified evaluation metric"""
+    # Compute specified evaluation metric"""
     f = metric_f.get(metric)
     if f is None:
         raise ValueError(f"Unknown metric name: {metric}")
